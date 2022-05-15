09df46aa446d5ca058c59dd0866a2cec8f307994
Vartencito <104112059+Vartencito@users.noreply.github.com>
2022-05-15 00:18:49 -0300

    Add files via upload

--- app-01/index6.js
+++ app-01/index6.js
@@ -1,1 +1,12 @@
-//fijarse cual es la bilioteca para instalar
+//biblioteca: country-to-currency
+
+import paisAMoneda from 'country-to-currency'
+
+const obtenerMoneda = (pais) => {
+    const moneda = paisAMoneda[pais];
+    return moneda;
+}
+
+
+let moneda = obtenerMoneda('AR');
+console.log(moneda);
